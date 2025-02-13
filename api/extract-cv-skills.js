4d533963873779aa57a4d4168f976a6af95560a3
Zoe Mwangi <gebruiker@Zoe-Mwangi.local>
2025-02-13 13:01:18 +0000

    bug fixed

--- api/extract-cv-skills.js
+++ api/extract-cv-skills.js
@@ -4,7 +4,7 @@ import path from "path";
 import OpenAI from "openai";
 import dotenv from "dotenv";
 import mammoth from "mammoth";
-import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf";
+import * as pdfjsLib from "pdfjs-dist";
 
 dotenv.config();
 
